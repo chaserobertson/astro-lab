c49fcbe2c985d054bde3dddb4efd8909d5fc762b
chase-robertson <chase.robertson@gametime.co>
2026-02-13 08:55:53 -0800

    merge duckdb dags into one serial

--- dags/duckdb_create.py
+++ dags/duckdb_create.py
@@ -1,36 +1,61 @@
 """
-This DAG shows how to use the DuckDB package directly in a @task decorated task.
+This DAG shows how its possible to use either the DuckDB python sdk or airflow connection to access a duck DB.
 """
 
-from airflow.sdk import dag, task, Asset
+from airflow.sdk import dag, task, chain, Asset
+from airflow.timetables.trigger import CronTriggerTimetable
 from pendulum import datetime
 import pandas as pd
 
-@dag(start_date=datetime(2023, 6, 1), schedule=None, catchup=False)
+TABLE_NAME = "color_rank"
+DUCKDB_FILE = f"include/{TABLE_NAME}.db"
+DUCKDB_CONN_ID = "my_local_duckdb_conn"
+
+@dag(
+    start_date=datetime(2023, 6, 1),
+    schedule=CronTriggerTimetable("0 18 * * *", timezone="UTC"),
+    catchup=False
+)
 def duckdb_create():
     @task
     def create_pandas_df():
         "Create a pandas DataFrame with toy data and return it."
-        ducks_in_my_garden_df = pd.DataFrame(
-            {"colors": ["blue", "red", "yellow"], "numbers": [2, 3, 4]}
+        import random
+        colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+        df = pd.DataFrame(
+            {"color": colors, "number": [random.randint(1, 15) for c in colors]}
         )
-        return ducks_in_my_garden_df
+        return df
 
-    @task(outlets=Asset('ducks_garden'))
-    def create_duckdb_table_from_pandas_df(ducks_in_my_garden_df: pd.DataFrame):
-        "Create a table in DuckDB based on a pandas DataFrame and query it"
+    @task(outlets=[Asset("color_rank")])
+    def create_duckdb_table_from_pandas_df(df: pd.DataFrame):
+        "Use the sdk to create a table in DuckDB based on a pandas DataFrame and query it"
         import duckdb
-        conn = duckdb.connect("include/my_garden_ducks.db")
-        conn.sql(
-            """CREATE TABLE IF NOT EXISTS ducks_garden AS 
-            SELECT * FROM ducks_in_my_garden_df;"""
-        )
+        conn = duckdb.connect(DUCKDB_FILE) # creates a new DB file if it doesn't already exist
+        conn.sql(f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} AS SELECT * FROM df;") # df accessible in sql via in-memory magic
 
-        sets_of_ducks = conn.sql("SELECT numbers FROM ducks_garden;").fetchall()
-        for ducks in sets_of_ducks:
-            print("quack " * ducks[0])
+        sets_of_ducks = conn.sql(f"SELECT color, number FROM {TABLE_NAME} ORDER BY number DESC;").fetchall()
+        for color, rank in sets_of_ducks:
+            print(f"{color}: " + "quack " * rank)
 
-    create_duckdb_table_from_pandas_df(ducks_in_my_garden_df=create_pandas_df())
+    @task
+    def query_duckdb():
+        "Use a connection and hook to retrieve the contents of the table"
+        from duckdb_provider.hooks.duckdb_hook import DuckDBHook
+        my_duck_hook = DuckDBHook.get_hook(DUCKDB_CONN_ID)
+        conn = my_duck_hook.get_conn()
+        r = conn.execute(f"SELECT * FROM {TABLE_NAME};").fetchall()
+        return r
+    
+    @task
+    def use_query_results(query_results):
+        print(query_results)
 
+    df = create_pandas_df()
+    db = create_duckdb_table_from_pandas_df(df)
+    q = query_duckdb()
+    res = use_query_results(q)
+
+    chain(df, db, q, res)
 
 duckdb_create()
